340288261ca6c826b6b079af8f2c943a438cb5a3
Axel Bankuna <axe.618@gmail.com>
2019-02-14 10:03:08 +0200

    Delete challenge4.js

--- challenge4.js
+++
@@ -1,7 +0,0 @@
-function hour_min(time) {
-    let hours = time / 60;
-    const min = time % 60;
-    return time + "is equal to: " + Math.floor(hours) + " and " + min + " minutes.";
-}
-
-console.log(hour_min(71));
